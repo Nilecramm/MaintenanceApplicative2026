ed14d6cc224c0ac297c5e4077d3572bf3a77db28
Nilecram <marcelin.ray@protonmail.com>
2026-04-26 12:58:14 +0200

    ✅ Refactor de Periodique pour utiliser les Value Objects

--- MyCalendar/projetJava/src/main/java/Event/Periodique.java
+++ MyCalendar/projetJava/src/main/java/Event/Periodique.java
@@ -1,15 +1,35 @@
 package Event;
 
+import domain.DureeEvenement;
+import domain.FrequenceRepetition;
+import domain.TitreEvenement;
+
 import java.time.LocalDateTime;
 
 public class Periodique extends Event {
-    public Periodique(String title, String proprietaire, LocalDateTime dateDebut, int dureeMinutes, String lieu, int frequenceJours) {
-        super("PERIODIQUE", title, proprietaire, dateDebut, dureeMinutes, lieu, null, frequenceJours);
+
+    private final FrequenceRepetition frequence;
+
+    public Periodique(TitreEvenement titre, String proprietaire, LocalDateTime dateDebut, DureeEvenement duree,
+                      FrequenceRepetition frequence) {
+        super(titre, proprietaire, dateDebut, duree);
+        this.frequence = frequence;
     }
 
     @Override
     public String description() {
-        return "Événement périodique : " + title + " tous les " + frequenceJours + " jours";
+        return "Événement périodique : " + titre + " tous les " + frequence + " jours";
     }
 
+    @Override
+    public boolean estDansPeriode(LocalDateTime debut, LocalDateTime fin) {
+        LocalDateTime temp = dateDebut;
+        while (temp.isBefore(fin)) {
+            if (!temp.isBefore(debut)) {
+                return true;
+            }
+            temp = temp.plusDays(frequence.value());
+        }
+        return false;
+    }
 }
